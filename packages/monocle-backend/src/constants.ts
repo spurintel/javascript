a0b0895543c75000879e9462510d8d22634aaa26
Jared Piedt <jared@spur.us>
2025-04-29 16:12:59 -0400

    use global consts

--- packages/monocle-backend/src/constants.ts
+++ packages/monocle-backend/src/constants.ts
@@ -1,4 +1,2 @@
-import { name, version } from '../package.json';
-
 export const API_URL = 'https://decrypt.mcl.spur.us';
-export const USER_AGENT = `${name}@${version}`;
+export const USER_AGENT = `${PACKAGE_NAME}@${PACKAGE_VERSION}`;
--- packages/monocle-backend/tsup.config.ts
+++ packages/monocle-backend/tsup.config.ts
@@ -16,5 +16,9 @@ export default defineConfig((overrideOptions) => {
     clean: true,
     minify: false,
     sourcemap: true,
+    define: {
+      PACKAGE_NAME: `"${name}"`,
+      PACKAGE_VERSION: `"${version}"`,
+    },
   };
 });
