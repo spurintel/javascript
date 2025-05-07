5d46ed3ce3117937598f64cb5c3826bec4ab64fa
Jared Piedt <jared@spur.us>
2025-05-07 17:48:25 -0400

    chore(monocle-backend): Upgrade dependencies

---
+++ .changeset/early-days-fix.md
@@ -0,0 +1,5 @@
+---
+"@spur.us/monocle-backend": patch
+---
+
+Upgrading jose@6.0.11
--- packages/monocle-backend/src/index.ts
+++ packages/monocle-backend/src/index.ts
@@ -1,4 +1,4 @@
-export { createMonocleClient } from './createMonocleClient';
-export type { MonocleClient } from './monocleClient';
+export { createMonocleClient } from './createMonocleClient.js';
+export type { MonocleClient } from './monocleClient.js';
 
 export type { MonocleAssessment } from '@spur.us/types';
